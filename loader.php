a9f17c577dec64dc6ee1944fb11ace4445979dc6
torbghancity <50368163+torbghancity@users.noreply.github.com>
2022-06-05 16:00:03 +0430

    add excel

--- loader.php
+++ loader.php
@@ -1,17 +1,3 @@
 <?php
 
 require(__DIR__ ."/vendor/autoload.php");
-
-
-/*
-spl_autoload_register(function ($class_name) {
-    
-    $filepath = __DIR__ . DIRECTORY_SEPARATOR .$class_name .'.php';
-    $filename = str_replace('\\', DIRECTORY_SEPARATOR, $filepath);
-
-    if (file_exists($filename)){
-        
-      require ($filename);
-
-    }
-});*/
